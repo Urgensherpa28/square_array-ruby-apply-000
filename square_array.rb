f005cf5a46b7dfb58f22e68cc0c1f2691a55eb2b
Urgen Sherpa <urgen.nyc@gmail.com>
2020-01-10 23:08:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,12 @@
 
 def square_array(array)
   # your code here
+  new_array = []
   counter = 0
   array.each do |numbers|
-    nums = numbers ** numbers
-    counter << nums
+    new_array << array[counter] ** 2
+    counter += 1
+    # nums = numbers ** numbers
+    # counter << nums
   end
 end
