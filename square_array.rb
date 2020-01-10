5c810304f22a5e083c656f76ea63a57aa40059a0
Urgen Sherpa <urgen.nyc@gmail.com>
2020-01-10 23:11:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,10 +4,11 @@ def square_array(array)
   new_array = []
   counter = 0
   array.each do |numbers|
-    new_array << array[counter] ** 2
-    counter += 1
-    # nums = numbers ** numbers
-    # counter << nums
+    # new_array << array[counter] ** 2
+    # counter += 1
+    nums = numbers ** numbers
+    counter << nums
+    new_array << counter
   end
   return new_array
 end
